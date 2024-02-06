b3912bbec5c60e5fe6313d4e633bcdd8023a4a08
Arundhuti Sur <arundhuti02@gmail.com>
2024-02-06 11:12:30 +0530

    initial

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -0,0 +1,105 @@
+package com.luv2code.springboot.thymeleafdemo.entity;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+
+
+@Entity
+@Table(name="employee_arundhuti")
+public class Employee {
+
+	// define fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Length(min=4, max=16, message="first name is invalid")
+	private String firstName;
+	
+	@Length(min=3, max=16, message="last name is invalid")
+	private String lastName;
+	
+	@Email(message = "invalid email",regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+	private String email;
+	
+		
+	// define constructors
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(int id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	// define getter/setter
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// define tostring
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+		
+}
+
+
+
+
+
+
+
+
+
+
+
